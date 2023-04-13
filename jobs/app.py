06080b4603edfa05b7c8577b836b65d587660710
Arghya-saha <Arghya-saha@users.noreply.github.com>
2023-04-13 11:21:28 +0530

    preparing dynamic content mod 4

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+import os
+
+PATH = os.path.join(os.getcwd(), 'db', 'jobs.sqlite')
 
 app = Flask(__name__)
 
@@ -7,3 +11,26 @@ app = Flask(__name__)
 
 def jobs():
     return render_template('index.html')
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+        connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+     if commit:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
